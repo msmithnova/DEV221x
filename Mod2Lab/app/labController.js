e16b9a73617aefb33461c8518ae9b249f8511147
Michael Smith <mikesmithnova@gmail.com>
2018-02-24 22:48:43 -0400

    Implement isolated scope

--- Mod2Lab/app/labController.js
+++ Mod2Lab/app/labController.js
@@ -3,9 +3,15 @@ angular.module('app')
     function() {
         var vm = this;
 
+        vm.show = show;
+
         vm.person = {
             name: 'Samuel Clemens',
             penName: 'Mark Twain'
         };
+
+        function show() {
+            alert(JSON.stringify(vm.person));
+        }
     }
 ]);
--- Mod2Lab/app/personDirective.js
+++ Mod2Lab/app/personDirective.js
@@ -1,9 +1,14 @@
 angular.module('app').directive('person', function () {
     return {
         scope: {
-            person: '='
+            person: '=',
+            action: '&',
         },
         restrict: 'E',
-        template: '<h3>{{person.name}}</h3><h4>{{person.penName}}</h4>'
+        //template: '<h3>{{person.name}}</h3><h4>{{person.penName}}</h4>'
+        template: 'Name: <input type="text" ng-model="person.name" class="form-control" />' +
+        'Pen Name: <input type="text" ng-model="person.penName" class="form-control" />' +
+        '<input type="button" ng-click="action()" value="Action" class="btn btn-primary"/>' + 
+        '<pre>{{person | json}}</pre>'
     };
 });
