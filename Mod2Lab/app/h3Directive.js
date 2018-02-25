aead7bd2d9463b26982a17296f61923838625349
Michael Smith <mikesmithnova@gmail.com>
2018-02-24 22:56:44 -0400

    Manipulating the DOM with Link Functions

--- Mod2Lab/app/h3Directive.js
+++ Mod2Lab/app/h3Directive.js
@@ -5,7 +5,17 @@ angular.module('app').directive('h3Directive', function() {
         },
 
         restrict: 'E',
-        template: '<h3>{{title}}</h3>'
+        replace: true,
+        template: '<h3>{{title}}</h3>',
+        link: function (scope, element, attrs) {
+            element.bind('mouseenter', function () {
+                element.css('background-color', 'red');
+            });
+
+            element.bind('mouseleave', function () {
+                element.css('background-color', 'silver');
+            });
+        }
     };
     
 });
